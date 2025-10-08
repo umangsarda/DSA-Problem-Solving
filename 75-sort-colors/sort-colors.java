6d767be547d6ac6f55595c3d94682987d5179aae
Umang Sarda <65851286+umangsarda@users.noreply.github.com>
2025-10-08 18:15:59 -0400

    Time: 1 ms (20.30%) | Memory: 42 MB (67.89%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -7,15 +7,14 @@ class Solution {
         int c0 = map.getOrDefault(0, 0);
         int c1 = map.getOrDefault(1, 0);
         int c2 = map.getOrDefault(2, 0);
-        int index=0;
         for(int i=0;i<c0;i++){
-            nums[index++]=0;
+            nums[i]=0;
         }
-        for(int i=0;i<c1;i++){
-            nums[index++]=1;
+        for(int i=c0;i<c0+c1;i++){
+            nums[i]=1;
         }
-        for(int i=0;i<c2;i++){
-            nums[index++]=2;
+        for(int i=c1+c0;i<nums.length;i++){
+            nums[i]=2;
         }
     }
 }
