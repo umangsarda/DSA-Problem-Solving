06a7396f9a22f624747440f2defc8035e8a771b8
Umang Sarda <65851286+umangsarda@users.noreply.github.com>
2025-12-02 19:09:29 -0500

    Time: 3 ms (73.15%) | Memory: 43.4 MB (19.19%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st= new Stack<>();
+        for(char c: s.toCharArray()){
+            if(c== '(' ||c== '{'||c== '['){
+                st.push(c);
+            }
+            else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                if(c== ')' && st.peek()=='(' || c== '}' && st.peek()=='{'|| c== ']' && st.peek()=='[' ){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
